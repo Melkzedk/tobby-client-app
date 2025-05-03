a081535145532ef58a736ad1cd8aacae5978cc12
unknown <melkywafula29@gmail.com>
2025-05-03 15:01:55 +0300

    more frontend updates

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
       <div className="container-fluid">
         <Link className="navbar-brand" to="/">MySite</Link>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
--- src/pages/Illustrations.js
+++ src/pages/Illustrations.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import image1 from '../Assets/Design1.jpg';
+import image1 from '../Assets/productphoto1.jpg';
 import image2 from '../Assets/Design2.jpg';
 import image3 from '../Assets/Design3.jpg';
 
 function Illustrations() {
   return (
-    <div className="container-fluid p-0 m-0 overflow-hidden">
+    <div className="container-fluid p-0 m-0 overflow-hidden illustrations-section">
       <div className="row g-0">
         <div className="col-9">
           <img src={image1} alt="Image 1" className="w-100 h-100 object-fit-cover" />
