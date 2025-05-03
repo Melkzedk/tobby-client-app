709b70191d1f3d73d12c336286cbc1ca1e68bcbf
unknown <melkywafula29@gmail.com>
2025-05-03 13:50:36 +0300

    changes

--- src/App.js
+++ src/App.js
@@ -1,58 +1,27 @@
+// src/App.js
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import About from './pages/About';
+import WhatWeDo from './pages/WhatWeDo';
+import Clients from './pages/Clients';
+import Contact from './pages/Contact';
 
 function App() {
   return (
-    <div>
-      {/* Navbar */}
-      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div className="container-fluid">
-          <a className="navbar-brand" href="#">MySite</a>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav ms-auto">
-              <li className="nav-item">
-                <a className="nav-link active" href="#">Home</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">About</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">What We Do</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">Clients</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="#">Contact Us</a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
-
-      {/* Image Layout */}
-      <div className="container-fluid px-0">
-        <div className="row g-0">
-          {[...Array(6)].map((_, index) => (
-            <div className="col-12 col-md-4" key={index}>
-              <img
-                src={`https://via.placeholder.com/600x400?text=Image+${index + 1}`}
-                alt={`layout-${index}`}
-                className="img-fluid w-100"
-              />
-            </div>
-          ))}
-        </div>
-      </div>
-
-      {/* Footer */}
-      <footer className="bg-dark text-white text-center py-3 mt-3">
-        &copy; 2025 MySite. All rights reserved.
-      </footer>
-    </div>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/what-we-do" element={<WhatWeDo />} />
+        <Route path="/clients" element={<Clients />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+      <Footer />
+    </Router>
   );
 }
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,17 +1,20 @@
-// src/pages/Home.js
 import React from 'react';
+import img1 from '../Assets/image1.jpg';
+import img2 from '../Assets/image2.jpg';
+import img3 from '../Assets/image3.jpg';
+import img4 from '../Assets/image4.jpg';
+import img5 from '../Assets/image5.jpg';
+import img6 from '../Assets/image6.jpg';
 
 function Home() {
+  const images = [img1, img2, img3, img4, img5, img6];
+
   return (
     <div className="container-fluid px-0">
       <div className="row g-0">
-        {[...Array(6)].map((_, index) => (
+        {images.map((image, index) => (
           <div className="col-12 col-md-4" key={index}>
-            <img
-              src={`https://via.placeholder.com/600x400?text=Image+${index + 1}`}
-              alt={`layout-${index}`}
-              className="img-fluid w-100"
-            />
+            <img src={image} alt={`layout-${index}`} className="img-fluid w-100" />
           </div>
         ))}
       </div>
