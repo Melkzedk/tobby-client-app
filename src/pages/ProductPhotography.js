010f68187f8367358321294330de053a53f18568
unknown <melkywafula29@gmail.com>
2025-05-04 08:20:04 +0300

    more frontend updates

--- src/pages/ProductPhotography.js
+++ src/pages/ProductPhotography.js
@@ -7,13 +7,26 @@ function ProductPhotography() {
   return (
     <div className="container-fluid px-0">
       <div className="row g-0">
-        <div className="col-12 col-md-6">
+        <div className="col-12 col-md-6 position-relative">
           <img
             src={image1}
             alt="Product 1"
             className="img-fluid w-100"
-            style={{ objectFit: 'cover', height: '80vh' }} // or 70vh, 60vh etc.
+            style={{ objectFit: 'cover', height: '80vh' }}
           />
+          <div 
+            className="position-absolute" 
+            style={{ 
+              bottom: 0, 
+              left: 0, 
+              backgroundColor: '#F7941D', 
+              padding: '15px 35px',
+              borderTopRightRadius: '20px',
+              zIndex: 10
+            }}
+          >
+            <h2 className="text-navy m-0 fw-bold">Product Photography</h2>
+          </div>
         </div>
         <div className="col-12 col-md-6">
           <img
