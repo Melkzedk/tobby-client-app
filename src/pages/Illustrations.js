828c61d8ba999c656e8919bc1215341e283f8790
unknown <melkywafula29@gmail.com>
2025-05-04 09:41:52 +0300

    more frontend updates

--- src/pages/Illustrations.js
+++ src/pages/Illustrations.js
@@ -5,15 +5,44 @@ import image3 from '../Assets/Design3.jpg';
 
 function Illustrations() {
   return (
-    <div className="container-fluid p-0 m-0 overflow-hidden illustrations-section">
-      <div className="row g-0">
+    <div className="container-fluid p-0 m-0 overflow-hidden illustrations-section position-relative">
+      <div className="row g-0" style={{ height: '50vh' }}>
         <div className="col-9">
-          <img src={image1} alt="Image 1" className="w-100 h-100 object-fit-cover" />
+          <img
+            src={image1}
+            alt="Image 1"
+            className="w-100 h-100"
+            style={{ objectFit: 'cover' }}
+          />
         </div>
         <div className="col-3 d-flex flex-column">
-          <img src={image2} alt="Image 2" className="w-100 flex-fill object-fit-cover" />
-          <img src={image3} alt="Image 3" className="w-100 flex-fill object-fit-cover" />
+          <img
+            src={image2}
+            alt="Image 2"
+            className="w-100 h-50"
+            style={{ objectFit: 'cover' }}
+          />
+          <img
+            src={image3}
+            alt="Image 3"
+            className="w-100 h-50"
+            style={{ objectFit: 'cover' }}
+          />
         </div>
+      </div>
+
+      {/* Illustrations text overlay */}
+      <div 
+        className="position-absolute text-dark fw-semibold px-3 py-2 rounded-2"
+        style={{ 
+          bottom: '15px', 
+          left: '15px',
+          backgroundColor: '#f8a21c', 
+          fontSize: '1.25rem',
+          zIndex: 10
+        }}
+      >
+        Illustrations
       </div>
     </div>
   );
