34d0c4c85e7b71796b34ac9c0b6bbc1c47f067b4
unknown <melkywafula29@gmail.com>
2025-05-05 15:13:43 +0300

    update

--- src/pages/Clothing.js
+++ src/pages/Clothing.js
@@ -12,7 +12,7 @@ function Clothing() {
       {/* Hero Banner Section */}
       <div className="position-relative" style={{ backgroundColor: '#FF7C43' }}>
         <div className="row g-0">
-          {/* Left Side - Orange Background with Person in Orange */}
+          {/* Left Side */}
           <div className="col-md-6 position-relative">
             <img 
               src={image1}
@@ -20,7 +20,6 @@ function Clothing() {
               className="w-100"
               style={{ objectFit: 'cover', height: '400px' }}
             />
-            {/* Overlay text */}
             <div 
               className="position-absolute text-dark fw-bold p-2 rounded-1"
               style={{ 
@@ -34,8 +33,8 @@ function Clothing() {
               Clothing
             </div>
           </div>
-          
-          {/* Right Side - Yellow Background with Person in White */}
+
+          {/* Right Side */}
           <div className="col-md-6 position-relative" style={{ backgroundColor: '#FFB347' }}>
             <div className="text-center pt-4">
               <p style={{ fontSize: '1.2rem', color: '#333' }}>
@@ -50,7 +49,7 @@ function Clothing() {
             />
           </div>
         </div>
-        
+
         {/* Social Media Icons */}
         <div className="d-flex justify-content-end p-2 bg-dark">
           {['linkedin', 'facebook', 'instagram', 'tiktok', 'youtube', 'twitter'].map((social, index) => (
@@ -74,59 +73,30 @@ function Clothing() {
           ))}
         </div>
       </div>
-      
+
       {/* Gallery Section */}
       <div className="py-4" style={{ backgroundColor: '#f5f5f5' }}>
         <div className="container">
           <h3 className="mb-4">Clothing</h3>
-          <div className="row g-3">
-            {/* Gallery Images */}
-            <div className="col-md-3">
-              <div style={{ backgroundColor: '#FF7C43', height: '200px' }}>
-                <img 
-                  src={image3}
-                  alt="Medical uniform" 
-                  className="w-100 h-100"
-                  style={{ objectFit: 'cover' }}
+
+          <div className="d-flex align-items-center">
+            {/* Left Arrow */}
+            <button className="btn p-2" style={{ fontSize: '2rem' }}>&lt;</button>
+
+            {/* 4 Images in one row, no gaps */}
+            <div className="d-flex flex-nowrap w-100">
+              {[image3, image4, image5, image6].map((img, index) => (
+                <img
+                  key={index}
+                  src={img}
+                  alt={`Gallery ${index}`}
+                  style={{ width: '25%', height: '200px', objectFit: 'cover' }}
                 />
-              </div>
+              ))}
             </div>
-            <div className="col-md-3">
-              <div style={{ backgroundColor: '#5DADE2', height: '200px' }}>
-                <img 
-                  src={image4}
-                  alt="Construction worker" 
-                  className="w-100 h-100"
-                  style={{ objectFit: 'cover' }}
-                />
-              </div>
-            </div>
-            <div className="col-md-3">
-              <div style={{ backgroundColor: '#333', height: '200px' }}>
-                <img 
-                  src={image5}
-                  alt="Barista uniform" 
-                  className="w-100 h-100"
-                  style={{ objectFit: 'cover' }}
-                />
-              </div>
-            </div>
-            <div className="col-md-3">
-              <div style={{ backgroundColor: '#3498DB', height: '200px' }}>
-                <img 
-                  src={image6}
-                  alt="Blue work shirt" 
-                  className="w-100 h-100"
-                  style={{ objectFit: 'cover' }}
-                />
-              </div>
-            </div>
-          </div>
-          
-          {/* Navigation Arrows */}
-          <div className="d-flex justify-content-between mt-3">
-            <button className="btn" style={{ fontSize: '2rem' }}>&lt;</button>
-            <button className="btn" style={{ fontSize: '2rem' }}>&gt;</button>
+
+            {/* Right Arrow */}
+            <button className="btn p-2" style={{ fontSize: '2rem' }}>&gt;</button>
           </div>
         </div>
       </div>
