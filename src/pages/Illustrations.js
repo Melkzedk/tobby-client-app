f8d98ed122e848260325ba663684f20858fc8ebc
unknown <melkywafula29@gmail.com>
2025-05-05 13:02:23 +0300

    more frontend updates

--- src/pages/Illustrations.js
+++ src/pages/Illustrations.js
@@ -5,41 +5,47 @@ import image3 from '../Assets/Design3.jpg';
 
 function Illustrations() {
   return (
-    <div className="container-fluid p-0 m-0 illustrations-section position-relative">
-      <div className="row g-0" style={{ height: '18vh' }}>
-        <div className="col-9">
+    <div className="container-fluid p-0 m-0 overflow-hidden illustrations-section position-relative">
+      <div
+        className="row g-0"
+        style={{
+          maxHeight: '300px',
+          height: '300px',
+        }}
+      >
+        <div className="col-9 col-sm-9 col-xxs-9" style={{ height: '100%' }}>
           <img
             src={image1}
-            alt="Senator design 1"
+            alt="abstract illustration 1"
             className="w-100 h-100"
             style={{ objectFit: 'cover' }}
           />
         </div>
-        <div className="col-3 d-flex flex-column">
+        <div className="col-3 col-sm-3 d-flex flex-column" style={{ height: '100%' }}>
           <img
             src={image2}
-            alt="Senator design 2"
-            className="w-100 h-50"
-            style={{ objectFit: 'cover' }}
+            alt="abstract illustration 2"
+            className="w-100"
+            style={{ height: '50%', objectFit: 'cover' }}
           />
           <img
             src={image3}
-            alt="Senator design 3"
-            className="w-100 h-50"
-            style={{ objectFit: 'cover' }}
+            alt="abstract illustration 3"
+            className="w-100"
+            style={{ height: '50%', objectFit: 'cover' }}
           />
         </div>
       </div>
 
-      {/* Illustrations text overlay */}
-      <div 
+      {/* Overlay text */}
+      <div
         className="position-absolute text-dark fw-semibold px-3 py-2 rounded-2"
-        style={{ 
-          bottom: '10px', 
+        style={{
+          bottom: '10px',
           left: '10px',
-          backgroundColor: '#f8a21c', 
-          fontSize: '0.9rem',
-          zIndex: 10
+          backgroundColor: '#f8a21c',
+          fontSize: '1rem',
+          zIndex: 10,
         }}
       >
         Illustrations
